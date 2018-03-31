265ea97ab55584a3a69d0fef23b122ae4048f135
Siddharth Jha <siddharthjha@outlook.com>
2018-04-01 01:53:58 +0530

    removed blender integration from all code except at one place, need to figure the edge_keys matrix out

--- attempt1.py
+++ attempt1.py
@@ -1,14 +1,18 @@
 import math
 import numpy as np
+import scipy.linalg
+import scipy.cluster
+import scipy.sparse
+import scipy.sparse.csgraph
 
 # Controls weight of geodesic to angular distance. Values closer to 0 give
 # the angular distance more importance, values closer to 1 give the geodesic
 # distance more importance
-delta = None
+delta = 0.03
 
 # Weight of convexity. Values close to zero give more importance to concave
 # angles, values close to 1 treat convex and concave angles more equally
-eta = None
+eta = 0.15
 
 def processfile(fname):
 	# Read an obj file and return its faces and vertices
@@ -56,7 +60,166 @@ def angular_distance(vertices_array, face1, face2):
 	use_eta = np.dot(face1_normal, vector_joining_centers) < 0
 	return use_eta , 1 - np.dot(face_normal(vertices_array, face1) , face_normal(vertices_array, face2))/(np.linalg.norm(face_normal(vertices_array, face1)) * np.linalg.norm(face_normal(vertices_array, face2)))
 
+def create_distance_matrices(faces_array, vertices_array):
+	# Creates the matrices of the angular and geodesic distances between all adjacent faces. 
+	# The i,j-th entry of the returned matrices contains the distance between the i-th and j-th face
+
+	faces = faces_array
+	l = len(faces_array)
+
+	# saves, which entries in A have to be scaled with eta
+	use_eta_list = []
+
+	# number of pairs of adjacent faces
+	num_adj = 0
+
+	# map from edge-key to adjacent faces
+	adj_faces_map = {}
+	# find adjacent faces by iterating edges
+	for index, face in enumerate(faces):
+		for edge in face.edge_keys:
+			if edge in adj_faces_map:
+				adj_faces_map[edge].append(index)
+			else:
+				adj_faces_map[edge] = [index]
+
+	# average G and cumulated A
+	avgG = 0
+	sumA = 0
+	# helping vectors to create sparse matrix later on
+	Arow = []
+	Acol = []
+	Aval = []
+	Grow = []
+	Gcol = []
+	Gval = []
+	# iterate adjacent faces and calculate distances
+	for edge, adj_faces in adj_faces_map.items():
+		if len(adj_faces) == 2:
+			i = adj_faces[0]
+			j = adj_faces[1]
+
+			Gtemp = _geodesic_distance(vertices_array, faces[i], faces[j], edge)
+			use_eta, Atemp = _angular_distance(vertices_array, faces[i], faces[j])
+			Gval.append(Gtemp)
+			Grow.append(i)
+			Gcol.append(j)
+			Gval.append(Gtemp)  # add symmetric entry
+			Grow.append(j)
+			Gcol.append(i)
+			Aval.append(Atemp)
+			Arow.append(i)
+			Acol.append(j)
+			Aval.append(Atemp)  # add symmetric entry
+			Arow.append(j)
+			Acol.append(i)
+
+			avgG += Gtemp
+			if use_eta:
+				# this entry has to be scaled with eta
+				use_eta_list.append((i,j))
+			else:
+				# doesn't need eta so add it to the sum, if we
+				# need eta we have to add it to the sum later
+				sumA += Atemp
+			num_adj += 1
+
+		elif len(adj_faces) > 2:
+			print("Edge with more than 2 adjacent faces: " + str(adj_faces) + "!")
+
+	# create sparse matrices
+	# matrix of geodesic distances
+	G = scipy.sparse.csr_matrix((Gval, (Grow, Gcol)), shape=(l, l))
+	# matrix of angular distances
+	A = scipy.sparse.csr_matrix((Aval, (Arow, Acol)), shape=(l, l))
+
+	avgG /= num_adj
+
+	return G, A, avgG, sumA, num_adj, use_eta_list
+
+def create_affinity_matrix(faces_array, vertices_array):
+	# Create the adjacency matrix of the given mesh
+	l = len(faces_array)
+	print("mesh_segmentation: Creating distance matrices...")
+	G, A, avgG, sumA, num_adj, use_eta_list = create_distance_matrices(faces_array, vertices_array)
+
+	# scale needed angular distances with eta
+	for indices in use_eta_list:
+		A[indices[0], indices[1]] *= eta
+		A[indices[1], indices[0]] *= eta
+		sumA += A[indices[0], indices[1]]
+	avgA = sumA/num_adj
+
+	# weight with delta and average value
+	G = G.dot(delta/avgG)
+	A = A.dot((1 - delta)/avgA)
+
+	print("mesh_segmentation: Finding shortest paths between all faces...")
+	# for each non adjacent pair of faces find shortest path of adjacent faces
+	W = scipy.sparse.csgraph.dijkstra(G + A, directed = False)
+	inf_indices = numpy.where(numpy.isinf(W))
+	W[inf_indices] = 0
+
+	print("mesh_segmentation: Creating affinity matrix...")
+	# change distance entries to similarities
+	sigma = W.sum()/(l ** 2)
+	den = 2 * (sigma ** 2)
+	W = numpy.exp(-W/den)
+	W[inf_indices] = 0
+	numpy.fill_diagonal(W, 1)
+
+	return W
+
+def initial_guess(Q, k):
+	# Computes an initial guess for the cluster-centers
+	n = Q.shape[0]
+	min_value = 2
+	min_indices=(-1,-1)
+	for (i,j), value in numpy.ndenumerate(Q):
+		if i != j and value < min_value:
+			min_value = Q[i,j]
+			min_indices = (i,j)
+	chosen = [min_indices[0], min_indices[1]]
+	for _ in range(2,k):
+		min_max = float("inf")
+		cur_max = 0
+		new_index = -1
+		for i in range(n):
+			if i not in chosen:
+				cur_max = Q[chosen,i].max()
+				if cur_max < min_max:
+					min_max = cur_max
+					new_index = i
+		chosen.append(new_index)
+	return chosen
+	
 filename = "data/bunny5k.obj"
 faces_array, vertices_array = processfile(filename) 
-dist = geodesic_distance(vertices_array, faces_array[1], faces_array[2], [1 , 2])
-print dist
+
+# affinity matrix
+W = create_affinity_matrix(faces_array, vertices_array)
+print("mesh_segmentation: Calculating graph laplacian...")
+# degree matrix
+Dsqrt = numpy.diag([math.sqrt(1/entry) for entry in W.sum(1)])
+# graph laplacian
+L = Dsqrt.dot(W.dot(Dsqrt))
+
+print("mesh_segmentation: Calculating eigenvectors...")
+# get eigenvectors
+l,V = scipy.linalg.eigh(L, eigvals = (L.shape[0] - k, L.shape[0] - 1))
+# normalize each column to unit length
+V = V / [numpy.linalg.norm(column) for column in V.transpose()]
+
+print("mesh_segmentation: Preparing kmeans...")
+# compute association matrix
+Q = V.dot(V.transpose())
+# compute initial guess for clustering
+initial_clusters = _initial_guess(Q, k)
+
+print("mesh_segmentation: Applying kmeans...")
+# apply kmeans
+cluster_res,_ = scipy.cluster.vq.kmeans(V, V[initial_clusters,:])
+# get identification vector
+idx,_ = scipy.cluster.vq.vq(V, cluster_res)
+
+print("mesh_segmentation: Done clustering!")
